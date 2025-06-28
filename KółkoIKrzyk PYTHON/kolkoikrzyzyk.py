39bce8de326e695a56615846280e2a253b59fa33
MattJ12344 <jaszczynskimati@gmail.com>
2025-06-28 19:41:43 +0200

    Na razie tyle zrobiłem z kółka i krzyżyka co prawda tylko wygrywa X ale postaram się to poprawić jutro

---
+++ KółkoIKrzyk PYTHON/kolkoikrzyzyk.py
@@ -0,0 +1,183 @@
+# metoda z graczami będzie dwóch graczy player 1 i palyer 2
+# metoda gdzie tworzy plansze 3 x 3 
+# metoda z wygraną i przegraną graczy : aby wygrać, trzeba mieć 3 wygrane
+# metoda przechowująca dane graczy
+# metoda sprawdzająca, aby gracze nie dawali swoich znaków na przeciwnego gracza znak
+# gracze x lub o, że x zaczyna pierwszy gracz, który ma x 
+# konstruktor z danymi
+
+import random
+from typing import List, Optional
+
+
+class KolkoIKrzyzyk:
+    def __init__(self) -> None:
+        self.rozmiar: int = 3
+        self.plansza: List[List[str]] = []
+        self.gracze: List[str] = ['X', 'O']
+        self.wygrany: Optional[str] = None
+        self.punkty: dict[str, int] = {'X': 0, 'O': 0}
+        self.utworzPustaPlansze()
+        self.rozpocznijGre()
+
+    def utworzPustaPlansze(self) -> None:
+        self.plansza = []
+        
+        for i in range(self.rozmiar):
+            wiersz: List[str] = []
+            
+            for j in range(self.rozmiar):
+                wiersz.append(' ')
+                
+            self.plansza.append(wiersz)
+    
+    
+    def wyswietlPlansze(self)-> None:
+        for wiersz in self.plansza:
+            print(' | '.join(wiersz))
+        
+        print('-' * (self.rozmiar * 4 - 1))
+        
+    
+    def znajdzWolnePole(self) -> List[int]:
+        wolnePola: List[List[int]] = []
+        
+        for i in range(self.rozmiar):
+            for j in range(self.rozmiar):
+                if self.plansza[i][j] == ' ':
+                    wolnePola.append([i, j])
+        
+        return random.choice(wolnePola)
+        
+        
+    
+    def wykonajRuch(self, gracz: str) -> None:
+        pole: List[int] = self.znajdzWolnePole()
+        
+        x = pole[0]
+        y = pole[1]
+        
+        self.plansza[x][y] = gracz
+        
+    def sprawdzWygrana(self, gracz: str) -> bool:
+        
+        for i in range(self.rozmiar):
+            wygrana_wiersz = True
+            
+            for j in range(self.rozmiar):
+                
+                if self.plansza[i][j] != gracz:
+                    
+                    wygrana_wiersz = False
+                    break
+                    
+            if wygrana_wiersz:
+                return True
+                
+                
+        for i in range(self.rozmiar):
+            
+            wygrana_kolumna = True
+            
+            for j in range(self.rozmiar):
+                
+                if self.plansza[j][i] != gracz:
+                    wygrana_kolumna = False
+                    break
+                    
+            if wygrana_kolumna:
+                return True
+                
+        
+        wygrana_przekatna1 = True
+        
+        for i in range(self.rozmiar):
+            
+            if self.plansza[i][i] != gracz:
+                
+                wygrana_przekatna1 = False
+                break
+                
+        if wygrana_przekatna1:
+            return True
+            
+            
+        wygrana_przekatna2 = True
+         
+        for i in range(self.rozmiar):
+            
+            if self.plansza[i][self.rozmiar - 1 - i] != gracz:
+                
+                wygrana_przekatna2 = False
+                break
+                
+        if wygrana_przekatna2:
+            return True
+
+        return False
+
+    def czyJestRemis(self) -> bool:
+        for i in range(self.rozmiar):
+            
+            for j in range(self.rozmiar):
+                
+                if self.plansza[i][j] == ' ':
+                    return False
+        
+        return True
+            
+    def zresetujRundeGry(self) -> None:
+        self.utworzPustaPlansze()
+        self.wygrany = None
+        
+    
+    def rozpocznijGre(self) -> None:
+        print("Start gry pomiędzy dwoma graczami")
+        
+        while self.punkty['X'] < 3 and self.punkty['O'] < 3:
+            self.zresetujRundeGry()
+            gracz = 'X'
+            
+            while True:
+                self.wykonajRuch(gracz)
+                
+                if self.sprawdzWygrana(gracz):
+                    self.wygrany = gracz
+                    self.punkty[gracz] += 1
+                    break
+                    
+                
+                if self.czyJestRemis():
+                    break
+                    
+                
+                if gracz == 'X':
+                    gracz = 'O'
+                    
+                else:
+                    gracz = 'X'
+                    
+                    
+            
+            print("\n Plansza po rundzie: ")
+            self.wyswietlPlansze()
+            
+            if self.wygrany != None:
+                print(f"Gracz {self.wygrany} wygrał rundę!")
+                
+            else:
+                print("Remis!")
+                
+            print(f"Wynik: X = {self.punkty['X']} | O = {self.punkty['O']}\n")
+            
+            
+        if self.punkty['X'] == 3:
+            print("Gracz X wygrał całą grę!")
+            
+        else:
+            print("Gracz O wygrał całą grę!")     
+
+
+gra = KolkoIKrzyzyk()
+
+gra.rozpocznijGre()
