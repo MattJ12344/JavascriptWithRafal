d17aa3fe9bd7479909435b83a1f08ba2ab452aa4
MattJ12344 <jaszczynskimati@gmail.com>
2025-07-19 15:28:43 +0200

    Wysyłam dwa pliki pythonowe jeden z testem drugi jako kod, to jest prawie te sam kod, ale z dodatkiem nowych artybutów jak ściana, nie chcialem zmieniać tam tego snake, więc skopiowałem kod i wrzuciłem jakodo do nowego i przy tym snake'u można przechodzić przez scianę oraz jest sciana na srodku planszy przez, którą może się skuć snake

---
+++ SNAKE PYTHON/SnakeBezRandoma/SnakeBezRandomu2ZeSciana.py
@@ -0,0 +1,164 @@
+
+from typing import List
+
+class Snake:
+    
+    # Konstruktor
+    def  __init__(self, rozmiar:int ) -> None:
+        self.rozmiar = rozmiar
+        self.plansza: List[List[str]] = []
+        self.snake: List[List[int]] = []
+        self.owoce: List[List[int]] = []
+        self.punkty: int = 0
+        self.koniecGry: bool = False
+        self.przeszkody: List[List[int]] = []
+    
+    
+    def wygenerujPlansze(self) -> None:
+        self.plansza = []
+        
+        for _ in range(self.rozmiar):
+            wiersz: List[str] = []
+            
+            for _ in range(self.rozmiar):
+                wiersz.append(' ')
+                
+            self.plansza.append(wiersz)
+
+    def wygenerujSnake(self, x:int, y: int) -> None:
+        
+        if 3<= self.rozmiar <= 4:
+            dlugosc: int = 1
+            
+        elif 5 <= self.rozmiar <=9:
+            dlugosc:int  = 2
+            
+        elif 10 <= self.rozmiar <= 20:
+            dlugosc = 3
+            
+        else:
+            dlugosc = 1
+            
+        self.snake = []
+        
+        for i in range(dlugosc):
+            nowyX:int = x
+            nowyY:int = y + i
+            
+            if nowyY < self.rozmiar:
+                self.snake.append([nowyX, nowyY])
+        
+        self.odswiezPlansze()
+            
+    def dodajPrzeszkodeNaSrodek(self) -> None:
+        srodek = self.rozmiar // 2
+        self.przeszkody.append([srodek, srodek])
+        self.plansza[srodek][srodek] = '#'         
+  
+        
+    def postawOwoc(self, x:int, y:int) -> None:
+        if [x, y] not in self.przeszkody:
+            self.owoce.append([x, y])
+            self.plansza[x][y] = 'O'
+    
+    def ileZjedzonychOwockow(self) -> int:
+        return self.punkty
+        
+    def dodajPunkty(self) -> None:
+        self.punkty += 1
+        
+    def wykonajRuchy(self, ruchyDoWykonania: List[str]) -> None:
+        
+        for kierunek in ruchyDoWykonania:
+            
+            if kierunek == 'w' or kierunek == 'W':
+                x = -1 
+                y = 0
+                
+            elif kierunek == 's' or kierunek == 'S':
+                x = 1 
+                y = 0
+                
+            elif kierunek == 'a' or kierunek == 'A':
+                x = 0 
+                y = -1
+            
+            elif kierunek == 'd' or kierunek == 'D':
+                x = 0 
+                y = 1
+                
+            else:
+                continue
+            
+            glowaX, glowaY = self.snake[len(self.snake) - 1]
+            
+            nowyX = (glowaX + x) % self.rozmiar
+            nowyY = (glowaY + y) % self.rozmiar
+            
+            
+            #szuka czy napotka czesc ciala i wtedy ma się zakonczyc gre
+            if [nowyX, nowyY] in self.snake: 
+                self.koniecGry = True
+                self.oznaczZakonczonaGre()
+                return
+            
+            if [nowyX, nowyY] in self.przeszkody: 
+                self.koniecGry = True
+                self.oznaczZakonczonaGre()
+                return
+            
+            self.snake.append([nowyX, nowyY])
+            
+            if [nowyX, nowyY] in self.owoce:
+                self.owoce.remove([nowyX, nowyY])
+                self.dodajPunkty()
+            
+            else:
+                self.snake.pop(0)
+                
+            self.odswiezPlansze()
+
+    
+    def oznaczZakonczonaGre(self) -> None:
+        for segment in self.snake:
+            x, y = segment
+            self.plansza[x][y] = 'Z'   
+            
+            
+    def odswiezPlansze(self) -> None:
+        
+        for i in range(self.rozmiar):
+            for j in range(self.rozmiar):
+                if self.plansza[i][j].isdigit() or self.plansza == 'O':
+                    self.plansza[i][j] = ' '
+        
+        for x, y in self.owoce:
+            if [x, y] not in self.przeszkody:
+                self.plansza[x][y] = 'O'
+            
+        for x, y in self.przeszkody:
+            self.plansza[x][y] = '#'
+        
+        for i, (x, y) in enumerate(reversed(self.snake)):
+            self.plansza[x][y] = str(i + 1)    
+    
+    
+    def planszaNaString(self) -> str:
+        wynik = ""
+        
+        for wiersz in self.plansza:
+            
+            for pole in wiersz:
+                
+                if pole == ' ':
+                    wynik = wynik + "[ ]"
+                    
+                else:
+                    wynik = wynik + f"[{pole}]"
+                    
+            wynik = wynik + "\n"
+            
+        return wynik
+            
+
+
---
+++ SNAKE PYTHON/SnakeBezRandoma/test do snakebezRandomu2 ze sciana.py
@@ -0,0 +1,79 @@
+from SnakeBezRandomu2ZeSciana import Snake
+from typing import List
+
+def test1():
+    gra = Snake(3)
+    gra.wygenerujPlansze()
+    gra.wygenerujSnake(0, 0)
+    gra.dodajPrzeszkodeNaSrodek()
+    gra.postawOwoc(0, 2)
+
+    poczatkowaPlansza1: str = (
+            "[1][ ][O]\n"
+            "[ ][#][ ]\n"
+            "[ ][ ][ ]\n"
+          
+        )
+
+    # print(gra.planszaNaString())
+    # print(poczatkowaPlansza1)
+
+    assert gra.planszaNaString() == poczatkowaPlansza1
+
+    ruchyDoWykonania: List[str] = ["d", "d", "D", "S"]
+    gra.wykonajRuchy(ruchyDoWykonania)
+
+    spodziewanaPlansza1: str = (
+            "[2][ ][ ]\n"
+            "[1][#][ ]\n"
+            "[ ][ ][ ]\n"
+        )
+
+    print("gra.planszaNaString() \n", gra.planszaNaString())
+    print("spodziewanaPlansza1 \n", spodziewanaPlansza1)
+
+    assert gra.planszaNaString() == spodziewanaPlansza1 , "Planasza zgadza się z spodziewaną"
+    assert gra.ileZjedzonychOwockow() == 1
+
+
+def test2():
+    gra = Snake(5)
+    gra.wygenerujPlansze()
+    gra.wygenerujSnake(0, 0)
+    gra.dodajPrzeszkodeNaSrodek()
+    gra.postawOwoc(4, 4)
+
+    poczatkowaPlansza2: str = (
+            "[2][1][ ][ ][ ]\n"
+            "[ ][ ][ ][ ][ ]\n"
+            "[ ][ ][#][ ][ ]\n"
+            "[ ][ ][ ][ ][ ]\n"
+            "[ ][ ][ ][ ][O]\n"
+            
+        )
+
+    # print(gra.planszaNaString())
+    # print(poczatkowaPlansza2)
+
+    assert gra.planszaNaString() == poczatkowaPlansza2
+
+    ruchyDoWykonania: List[str] = ["d", "d", "s", "s", "s", "s", "d", "d", "w", "W"]
+    gra.wykonajRuchy(ruchyDoWykonania)
+
+    spodziewanaPlansza2: str = (
+            "[ ][ ][ ][ ][ ]\n"
+            "[ ][ ][ ][ ][ ]\n"
+            "[1][ ][#][ ][ ]\n"
+            "[2][ ][ ][ ][ ]\n"
+            "[3][ ][ ][ ][ ]\n"
+        )
+
+    print("gra.planszaNaString() \n", gra.planszaNaString())
+    print("spodziewanaPlansza2 \n", spodziewanaPlansza2)
+
+    assert gra.planszaNaString() == spodziewanaPlansza2 , "Planasza zgadza się z spodziewaną"
+    assert gra.ileZjedzonychOwockow() == 1
+
+
+test1()
+test2()
